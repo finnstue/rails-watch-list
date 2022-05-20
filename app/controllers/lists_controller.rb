b77e975528a0fb38e0ce6d2f49ec6d787e1aff7f
finn stuerenburg <finnstueren@gmail.com>
2022-05-20 11:58:19 +0200

    commit for heroku

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,36 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  # POST /lists
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,22 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :destroy]
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_url, notice: 'Movie was successfully destroyed.'
+  end
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 end
